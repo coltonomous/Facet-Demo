ce8be944842c182048f2ffa4a3bc8f60b7f7e677
Colton McDavid <coltonomous@users.noreply.github.com>
2020-06-29 01:02:20 -0700

    Delete credentials.js

--- server/constants/credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports.nexmoAPIKey = '00d7db3c';
-module.exports.nexmoAPISecret = 'oanctriTDcO5PuLW';
-module.exports.nexmoNumber = '15126612257';
-
-module.exports.emailAddress = 'chuckfin50@gmail.com';
-module.exports.emailPassword = 'majortomtvc15';
